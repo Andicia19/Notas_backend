89c7676ee6c239c720307a291824165d7f346cde
Andres Guzman <andresfgg1904@gmail.com>
2025-03-01 15:55:16 -0500

    Agrgar modos

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,11 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+
+//selecciona la base de datos segun el modo, yo uso la misma para no crear otra
+const MONGODB_URI = process.env.NODE_ENV === 'test' 
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
